967bfee3b9229af5d2b05955f5995b1325427c5b
Marián Konček <marian.koncek@mailbox.org>
2025-09-05 15:29:53 +0200

    Add tmt plugin for mock provisioning

---
+++ mock/py/mockbuild/plugins/tmt.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python3
+# python library imports
+
+import logging
+import subprocess
+import os
+import shlex
+import socket
+import sys
+
+import pathlib
+
+from io import StringIO
+
+from mockbuild import file_util
+from mockbuild.exception import Error
+from mockbuild.trace_decorator import traceLog
+
+requires_api_version = "1.1"
+
+# plugin entry point
+@traceLog()
+def init(plugins, conf, buildroot):
+    TmtPlugin(plugins, conf, buildroot)
+
+
+class TmtPlugin(object):
+    """
+    Enables tmt to work with mock shell
+    """
+
+    @traceLog()
+    def __init__(self, plugins, conf, buildroot):
+        self.buildroot = buildroot
+        self.config = conf
+        self.workdir = pathlib.Path(self.buildroot.basedir).joinpath("workdir").resolve()
+        self.workdir_root = None
+        self.host_workdir_root = None
+
+        if self.buildroot.bootstrap_buildroot is not None:
+            plugins.add_hook("preshell", self.mount_overlay)
+
+        plugins.add_hook("preshell", self.mount_workdir)
+        plugins.add_hook("postshell", self.umount_workdir)
+
+        if self.buildroot.bootstrap_buildroot is not None:
+            plugins.add_hook("postshell", self.umount_overlay)
+
+    @traceLog()
+    def mount_overlay(self):
+        self.buildroot.root_log.info("Enabled tmt plugin")
+        
+        forbidden_prefixes = [
+            "/bin",
+            "/boot",
+            # "/dev",
+            "/etc",
+            # "/home",
+            "/lib",
+            "/lib64",
+            # "/media",
+            # "/mnt",
+            # "/opt",
+            "/proc",
+            # "/root",
+            # "/run",
+            "/sbin",
+            # "/srv",
+            "/sys",
+            # "/tmp",
+            "/usr",
+            # "/var",
+        ]
+        
+        for forbidden_prefix in forbidden_prefixes:
+            if pathlib.Path(self.buildroot.basedir).is_relative_to(forbidden_prefix):
+                raise Error(f"Refusing to mount overlay in directory: {self.buildroot.basedir}")
+        
+        # overlay bootstrap chroot into chroot
+        subprocess.run(['rm', '-rf', self.workdir], check = True)
+        os.mkdir(self.workdir)
+        subprocess.run(["mount", "-t", "overlay", "overlay",
+            "-o", f"lowerdir={self.buildroot.bootstrap_buildroot.rootdir}",
+            "-o", f"upperdir={self.buildroot.rootdir}",
+            "-o", f"workdir={self.workdir}",
+            str(self.buildroot.rootdir)], check = True)
+        
+    @traceLog()
+    def mount_workdir(self):
+        self.workdir_root = str(self.config["workdir_root"])
+        self.host_workdir_root = self.buildroot.rootdir + self.workdir_root
+        # bind-mount tmt workdir into chroot
+        subprocess.run(['rm', '-rf', self.host_workdir_root, self.buildroot.rootdir + '/tmp/unmount-bootstrap-chroot'], check = True)
+        os.mkdir(self.host_workdir_root)
+        subprocess.run(["mount", "--bind", self.workdir_root, self.host_workdir_root], check = True)
+    
+    @traceLog()
+    def umount_overlay(self):
+        subprocess.run(["umount", "-l", str(self.buildroot.rootdir)], check = True)
+    
+    @traceLog()
+    def umount_workdir(self):
+        subprocess.run(["umount", "-l", self.host_workdir_root], check = True)
